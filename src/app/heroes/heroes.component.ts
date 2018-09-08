3bb94ca8989d786a703f5ce1f94b0ffad5705808
Chris <chewning.cl@gmail.com>
2018-09-07 23:17:11 -0500

    part 3 finished

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+//NOTE: when you type in your bash (CLI, command line interface in Angular):  ng generate component <name>   you get 4 files and an update. example:
+// CREATE src/app/doggos/doggos.component.css (0 bytes)
+// CREATE src/app/doggos/doggos.component.html (25 bytes)
+// CREATE src/app/doggos/doggos.component.spec.ts (628 bytes)
+// CREATE src/app/doggos/doggos.component.ts (269 bytes)
+// UPDATE src/app/app.module.ts (478 bytes)
+
+//app.module.ts is like a .json file in a way. declarations (dependencies in .json). If you delete your module you should also delete it in the module.ts
+
+//for this one, I typed ng generate component <heroes>
+
+//diff b.w components & modules: Components control views (html). They also communicate with other components and services to bring functionality to your app.
+
+//Modules consist of one or more components... Modules allow you to manage your components to bring modularity (degree which components can be separated & combined) to your app.
+//https://stackoverflow.com/questions/40073941/whats-the-difference-between-an-angular-component-and-module
+
+
+
+@Component({
+  selector: 'app-heroes', //component's css selector.
+  templateUrl: './heroes.component.html', //location of the component's template file.
+  styleUrls: ['./heroes.component.css'] //location of the component's private css styles.
+})
+
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() { //lifecycle hook used for heavy init logic & fetching data. like React's componentDidMount. you used
+  }
+
+}
